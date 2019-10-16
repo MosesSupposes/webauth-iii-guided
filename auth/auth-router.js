f67b4c9a374c9487d18d3b1ae53f59e0d5293565
Moses Samuel <mosamuel98@gmail.com>
2019-10-16 11:53:43 -0700

    fix bug that was causing requests with valid tokens to be denied

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken')
+const secrets = require('../config/secrets')
 
 const Users = require('../users/users-model.js');
 
@@ -47,10 +48,9 @@ function generateToken(user) {
     username: user.username,
     subject: user.id
   }
-  const secret = 'this is not secret'
-  const options = { expiresIn: '1h'}
+  const options = { expiresIn: '1h' }
 
-  return jwt.sign(payload, secret, options)
+  return jwt.sign(payload, secrets.jwtSecret, options)
 }
 
 module.exports = router;
